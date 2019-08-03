d8502b7ed3ed466d8c9ba77d39a2e2ed5c981f55
Dimpho PUTU <diputu@c4r16s11.wethinkcode.co.za>
2019-08-03 13:35:54 +0200

    yo

--- src/main/java/za/co/diputu/view/PlayMission.java
+++ src/main/java/za/co/diputu/view/PlayMission.java
@@ -9,14 +9,25 @@ public class PlayMission {
 
     public void StartMission()
     {
+//        String[] coordinates = {"North", "East", "West", "South"};
+        ArrayList<String> coordinates = new ArrayList<String>();
+        int i= 1;
 
+        coordinates.add("North");
+        coordinates.add("East");
+        coordinates.add("West");
+        coordinates.add("South");
+//        coordinates.forEach((n) -> System.out.println(n));
+        for (String x : coordinates){
+            System.out.println("[" + i + "]" + coordinates);
+        }
     }
 
     public void StartOption(){
         String obj = n.nextLine();
         if (obj == "1")
         {
-
+            StartMission();
         }
         System.out.println("Press 1 to start game");
     }
