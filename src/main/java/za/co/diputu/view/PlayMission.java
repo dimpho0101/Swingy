968d9e4b73eca90917232c8c9c882bc6eb3af836
Dimpho PUTU <diputu@c4r16s11.wethinkcode.co.za>
2019-08-03 14:53:10 +0200

    yo

--- src/main/java/za/co/diputu/view/PlayMission.java
+++ src/main/java/za/co/diputu/view/PlayMission.java
@@ -12,19 +12,25 @@ public class PlayMission {
 //        String[] coordinates = {"North", "East", "West", "South"};
         ArrayList<String> coordinates = new ArrayList<String>();
         int index;
+        int i;
 
         coordinates.add("North");
         coordinates.add("East");
         coordinates.add("West");
         coordinates.add("South");
 //        coordinates.forEach((n) -> System.out.println(n));
-        for (int i= 0;i < 4;i++)
+        for (i= 0;i < 4;i++)
         {
             System.out.println("[" + i + "]" + coordinates.get(i).toString());
         }
         index = Integer.parseInt(n.nextLine());
-        if (){
 
+        int j = 0;
+        for (String u : coordinates) {
+            if (i == index + 1) {
+                System.out.println("You selected " + u + " as a coordinate");
+            }
+            j++;
         }
     }
 
